1ab8fc99c307453c0423fd2da452211766bb4fbf
Jina Park <jinapark119@gmail.com>
2023-03-20 03:27:20 +0000

    Update lock_directories.c

--- lock_directories.c
+++ lock_directories.c
@@ -24,18 +24,18 @@ void lock_directories() {
 		execvp(args[0], args); // execute chmod
 
 	} else { // parent
-        int status; // status of child process
+        	int status; // status of child process
 
-        if(wait(&status) == -1) { // if wait fails
-            syslog(LOG_ERR, "lock_directories: wait() failed"); // log error
-        } // end if
+        	if(wait(&status) == -1) { // if wait fails
+            		syslog(LOG_ERR, "lock_directories: wait() failed"); // log error
+       		 } // end if
 
-        if( (WIFEXITED(status)) ) { // if child process exited normally
-            if( WEXITSTATUS(status) != 0 ) { // if child process exited with error
-                syslog(LOG_ERR, "ERROR LOCKING DIRECTORIES, STATUS %d", WEXITSTATUS(status)); // log error
-            } else { // if child process exited successfully
-                syslog(LOG_INFO, "DIRECTORIES HAVE BEEN LOCKED UNTIL BACKUP AND TRANSFER COMPLETES"); // log success
-            } // end if else
-        } // end if
+        	if( (WIFEXITED(status)) ) { // if child process exited normally
+            		if( WEXITSTATUS(status) != 0 ) { // if child process exited with error
+                		syslog(LOG_ERR, "ERROR LOCKING DIRECTORIES, STATUS %d", WEXITSTATUS(status)); // log error
+           		 } else { // if child process exited successfully
+                		syslog(LOG_INFO, "DIRECTORIES HAVE BEEN LOCKED UNTIL BACKUP AND TRANSFER COMPLETES"); // log success
+            		} // end if else
+       		 } // end if
 	} // end if else
 } // end lock_directories
